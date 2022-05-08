8dbb16a2adcbf98516a52ae382097c77b842b34e
Dorrwa <pdorr@frba.utn.edu.ar>
2022-05-08 15:07:46 -0300

    agrego gitignore ya esta funcionando

--- back.py
+++ back.py
@@ -2,9 +2,36 @@
 # import necessary libraries and functions
 from flask import Flask, jsonify, request
 from apscheduler.schedulers.background import BackgroundScheduler# creating a Flask app
-#app = Flask(__name__)
+app = Flask(__name__)
 import RPi.GPIO as GPIO
 import time
+import os
+
+hostnames = ["google.com","192.168.0.1"]
+pins=[20,30]
+
+def ping(num):
+    response = os.system("ping -c 1 " + hostnames[num])
+    if response == 0:
+        print(hostnames[num], 'is up!')
+        return False
+    else:
+        print(hostnames[num], 'is down!')
+        return True
+def enviarPulsoDe(segs,rignum):
+    GPIO.output(pins[rignum], GPIO.HIGH)
+    time.sleep(segs)
+    GPIO.output(pins[rignum], GPIO.LOW)
+
+def checkSensors():
+    for i in range(len(hostnames)):
+        if ping(i):
+            enviarPulsoDe(5,i)
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(checkSensors(),'interval',seconds=300)
+sched.start()
+"""
 LED_PIN = 20
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
@@ -13,7 +40,6 @@ GPIO.output(LED_PIN, GPIO.HIGH)
 time.sleep(2)
 GPIO.output(LED_PIN, GPIO.LOW)
 def sensor():
-    """ Function for test purposes. """
     GPIO.setup(LED_PIN, GPIO.OUT)
     print("prender")
     GPIO.output(LED_PIN, GPIO.HIGH)
@@ -25,15 +51,35 @@ def sensor():
 sched = BackgroundScheduler(daemon=True)
 sched.add_job(sensor,'interval',seconds=5)
 sched.start()
-@app.route('/login', methods=['GET', 'POST'])
-def home():
+"""
+@app.route('/turnon/<int:num>', methods=['GET'])
+def turnon(num):
     if (request.method == 'GET'):
-        data = "hello world"
+        data = "Recibido"
+        print(data)
+        enviarPulsoDe(1,num)
         return jsonify({'data': data})
     if (request.method == 'POST'):
         data = request.get_json()
         return data
-@app.route('/home/<int:num>', methods=['GET'])
+@app.route('/turnoff/<int:num>', methods=['GET'])
+def turnoff(num):
+    if (request.method == 'GET'):
+        data = "Recibido"
+        enviarPulsoDe(5, num)
+        return jsonify({'data': data})
+    if (request.method == 'POST'):
+        data = request.get_json()
+    return data
+@app.route('/reset/<int:num>', methods=['GET'])
+def reset(num):
+    if (request.method == 'GET'):
+        data = "Recibido"
+        enviarPulsoDe(5, num)
+        time.sleep(1)
+        enviarPulsoDe(1,num)
+        return jsonify({'data': data})
+
 def disp(num):
     return jsonify({'data': num ** 2})
 # driver function
--- botTg.py
+++ botTg.py
@@ -5,45 +5,8 @@ from telegram.ext import (Updater, CommandHandler)
 from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 import pandas as pd
 from datetime import datetime
-
-def mesActual(update, context):
-    client = Client(config.API_KEY, config.API_SECRET)
-    total = 0
-    dphistory = client.get_deposit_history(coin="ETH")
-    df = pd.DataFrame(dphistory,
-                      columns=['amount', 'coin', 'network', 'status', 'address', 'addressTag', 'txId', 'insertTime',
-                               'transferType', 'unlockConfirm'])
-    df.insertTime = df.insertTime.apply(lambda x: pd.to_datetime(x, utc=True, unit='ms'))
-    for index, row in df.iterrows():
-        amount = row["amount"]
-        fecha = row["insertTime"]
-        coin = row["coin"]
-        address = row["address"]
-        if fecha.month == (datetime.now().month )  and coin=='ETH':
-            total += float(amount)
-
-    mensaje = "En el mes " + str(datetime.now().month) + " vamos acumulando un total de: " + str(total) + " ETH"
-    context.bot.send_message(update.message.chat_id, mensaje)
-def zil(update, context):
-    client = Client(config.API_KEY, config.API_SECRET)
-    total = 0
-    dphistory = client.get_deposit_history(coin="ZIL")
-    df = pd.DataFrame(dphistory,
-                      columns=['amount', 'coin', 'network', 'status', 'address', 'addressTag', 'txId', 'insertTime',
-                               'transferType', 'unlockConfirm'])
-    df.insertTime = df.insertTime.apply(lambda x: pd.to_datetime(x, utc=True, unit='ms'))
-    for index, row in df.iterrows():
-        amount = row["amount"]
-        fecha = row["insertTime"]
-        coin = row["coin"]
-        address = row["address"]
-        if coin=='ZIL':
-            total += float(amount)
-
-    mensaje = " vamos acumulando un total de: " + str(total) + " ZIL"
-    context.bot.send_message(update.message.chat_id, mensaje)
-
-def mesAnterior(update, context):
+import requests
+"""def mesAnterior(update, context):
     client = Client(config.API_KEY, config.API_SECRET)
     total = 0
     dphistory = client.get_deposit_history(coin="ETH")
@@ -60,17 +23,26 @@ def mesAnterior(update, context):
             total += float(amount)
     mensaje= "En el mes "+str(datetime.now().month-1)+ " acumulamos un total de: " + str(total) + " ETH"
     context.bot.send_message(update.message.chat_id, mensaje)
+"""
+def turnon(update,context):
+    try:
+        print(context.args[0])
+        mensaje = str(context.args[0])
+        response = requests.get("http://127.0.0.1:5000/turnon/"+str(context.args[0]))
+        print(response)
+        context.bot.send_message(update.message.chat_id, mensaje)
+    except:
+        context.bot.send_message(update.message.chat_id, "Ingrese un valor")
 def help(update, context):
-    context.bot.send_message(update.message.chat_id, "Lista de comandos \n /mesActual - Total ETH en el mes \n /mesAnterior - Total ETH en el mes anterior")
+    context.bot.send_message(update.message.chat_id, "Lista de comandos \n /turnon + numero - prender rig numero x \n /turnoff + numero - apagar rig numero x \n /restart numero - restartea el rig")
 def tgbot():
     TOKEN = config.TGTOKEN
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
     # Eventos que activarán nuestro bot.
-    dp.add_handler(CommandHandler('mesActual', mesActual))
-    dp.add_handler(CommandHandler('mesAnterior', mesAnterior))
-    dp.add_handler(CommandHandler('zil', zil))
+    #dp.add_handler(CommandHandler('mesAnterior', mesAnterior))
+    dp.add_handler(CommandHandler('turnon', turnon))
     dp.add_handler(CommandHandler('help', help))
     # Comienza el bot
     updater.start_polling()
